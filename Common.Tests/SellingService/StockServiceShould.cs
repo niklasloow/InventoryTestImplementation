d51cc98441f9d8fbd4718e1a0fd4f53ce13ffbeb
Niklas Lööw <niklas.loow@cdon.com>
2021-04-23 15:10:49 +0200

    Stock service and tests

---
+++ Common.Tests/SellingService/StockServiceShould.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Better.RandomDataGenerator;
+using Common.Models;
+using Common.StorageRepository;
+using FakeItEasy;
+using Xunit;
+
+namespace Common.SellingService
+{
+    public class StockServiceShould
+    {
+        private readonly IStorageRepository<Article> _articleRepository;
+        private readonly IStorageRepository<Product> _productRepository;
+        private readonly StockService _sut;
+        public StockServiceShould()
+        {
+            _articleRepository = A.Fake<IStorageRepository<Article>>();
+            _productRepository = A.Fake<IStorageRepository<Product>>();
+            _sut = new StockService(_articleRepository, _productRepository);
+        }
+
+        [Fact]
+        public void CallUpdateArticleRepository()
+        {
+            //Arrange
+            var articleId = RandomDataGenerator.StringGenerator.GetRandomTinyString();
+            var amountOf = RandomDataGenerator.IntGenerator.GetRandomNumberInRange(1, 50);
+            var product = new ProductBuilder()
+                .Default()
+                .WithContainingArticle(articleId, amountOf)
+                .Build();
+
+            var article = new ArticleBuilder()
+                .Default()
+                .TypicalArticle()
+                .Build();
+
+            var productId = RandomDataGenerator.StringGenerator.GetRandomTinyString();
+            A.CallTo(() => _productRepository.GetById(A<string>.Ignored)).Returns(product); 
+            A.CallTo(() => _articleRepository.GetById(A<string>.Ignored)).Returns(article); 
+            
+            //Act
+            _sut.DecreaseArticleStockForProduct(productId);
+            
+            //Assert
+            A.CallTo(() => _articleRepository.Update(article)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void LowerStockAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void WorkWithFaultyData()
+        {
+            var productId = RandomDataGenerator.StringGenerator.GetRandomTinyString();
+            A.CallTo(() => _productRepository.GetById(A<string>.Ignored)).Returns(null);
+            A.CallTo(() => _articleRepository.GetById(A<string>.Ignored)).Returns(null);
+
+            _sut.DecreaseArticleStockForProduct(productId);
+
+            A.CallTo(() => _articleRepository.Update(A<Article>.Ignored)).MustHaveHappened();
+        }
+    }
+}
---
+++ Common/SellingService/StockService.cs
@@ -0,0 +1,38 @@
+﻿using Common.Models;
+using Common.StorageRepository;
+
+namespace Common.SellingService
+{
+    public class StockService : IStockService
+    {
+        private readonly IStorageRepository<Article> _articleRepository;
+        private readonly IStorageRepository<Product> _productRepository;
+        
+        public StockService(IStorageRepository<Article> articleRepository, 
+            IStorageRepository<Product> productRepository)
+        {
+            _articleRepository = articleRepository;
+            _productRepository = productRepository;
+        }
+
+        public void DecreaseArticleStockForProduct(string productId)
+        {
+            var product = _productRepository.GetById(productId);
+            foreach (var article in product.ContainArticles)
+            {
+                DecreaseStock(article.ArtId, article.AmountOf);
+            }
+        }
+
+        private void DecreaseStock(string artId, int amount) => UpdateArticleStock(artId, -amount);
+        
+        private void IncreaseStock(string artId, int amount) => UpdateArticleStock(artId, amount);
+
+        private void UpdateArticleStock(string artId, int amount)
+        {
+            var part = _articleRepository.GetById(artId);
+            part.Stock -= amount;
+            _articleRepository.Update(part);
+        }
+    }
+}
