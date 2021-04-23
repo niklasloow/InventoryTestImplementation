0101f4a0ee54ed9093b3ead12286a323dbfe264d
Niklas Lööw <niklas.loow@cdon.com>
2021-04-23 15:13:21 +0200

    Automapper and DI

---
+++ Common/AutomapperProfiles/ArticleInventoryProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Common.Models;
+
+namespace Common.AutomapperProfiles
+{
+    public class ArticleInventoryProfile : Profile
+    {
+        public ArticleInventoryProfile()
+        {
+            CreateMap<Article, InventoryDto>()
+                .ForMember(dest => dest.ArtId, source =>
+                    source.MapFrom(src => src.ArtId))
+                .ForMember(dest => dest.Stock, source =>
+                    source.MapFrom(src => src.Stock))
+                .ForMember(dest => dest.Name, source =>
+                    source.MapFrom(src => src.Name));
+
+        }
+    }
+}
---
+++ Common/DependancyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Common.AutomapperProfiles;
+using Common.Models;
+using Common.StorageRepository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common
+{
+    public static class DependencyInjectionCommon
+    {
+        public static void AddCommonInjection(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IBaseStorageRepository<InventoryListDto>, FileStorageBaseRepository<InventoryListDto>>(sp
+                => new FileStorageBaseRepository<InventoryListDto>(FileNameConstants.Inventory));
+
+            serviceCollection.AddScoped<IBaseStorageRepository<InventoryListDto>, FileStorageBaseRepository<InventoryListDto>>(sp
+                => new FileStorageBaseRepository<InventoryListDto>(FileNameConstants.Products));
+            serviceCollection.AddAutoMapper(typeof(ArticleInventoryProfile));
+        }
+
+    }
+
+    public static class FileNameConstants
+    {
+        public static string Inventory => "inventory";
+        public static string Products => "products";
+    }
+}
