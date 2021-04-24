b341e7d41fd87671fa7265a72e3da4c52ab24df3
Niklas Lööw <niklas.loow@cdon.com>
2021-04-24 21:07:08 +0200

    Startup for AF

---
+++ Host.Azurefunction/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Common;
+using Host.AzureFunction;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Host.AzureFunction
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+#if DEBUG
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: false)
+#endif
+                .AddEnvironmentVariables()
+                .Build();
+            builder.Services.AddCommonInjection();
+
+        }
+    }
+}
