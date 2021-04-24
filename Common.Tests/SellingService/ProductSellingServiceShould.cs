e2a62a032101a00799f11a73099cff392152829f
Niklas Lööw <niklas.loow@cdon.com>
2021-04-24 21:09:06 +0200

    Remove placeholder code

--- Common.Tests/SellingService/ProductSellingServiceShould.cs
+++ Common.Tests/SellingService/ProductSellingServiceShould.cs
@@ -7,28 +7,6 @@ namespace Common.SellingService
 {
     public class ProductSellingServiceShould
     {
-        private readonly IStockService _stockService;
-        private readonly IStorageRepository<Product> _productRepository;
-
-        public ProductSellingServiceShould()
-        {
-            //_stockService = stockService;
-            //_productRepository = productRepository;
-        }
         
-        public bool CanSellProduct(string productId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SellProduct(string productId)
-        {
-            _stockService.DecreaseArticleStockForProduct(productId);
-        }
-
-        public IEnumerable<Product> GetAllSellableProduct()
-        {
-            return _productRepository.GetAll();
-        }
     }
 }
