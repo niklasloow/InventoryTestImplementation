734461a845e4e28babb8bfdba1432300d6428087
Niklas Lööw <niklas.loow@cdon.com>
2021-04-26 22:51:37 +0200

    First pieces of calc stock in place

--- Common/Models/Product.cs
+++ Common/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace Common.Models
@@ -13,6 +14,7 @@ namespace Common.Models
 
     public class Product
     {
+
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
@@ -24,6 +26,27 @@ namespace Common.Models
 
         [JsonPropertyName("price")]
         public decimal Price { get; set; }
+
+        [JsonPropertyName("stock")]
+        public decimal Stock => this.CalculateStock();
+    }
+
+    public static class ProductHelper
+    {
+        public static int CalculateStock(this Product product)
+        {
+            //TODO need to fetch article, and cant be a static helper then
+            var article = new Article
+            {
+                ArtId = "",
+                Stock = 20,
+                Name = ""
+            };
+            var values = product.ContainArticles.Select(
+                containArticle => article.Stock / containArticle.AmountOf).ToList();
+
+            return values.OrderBy(p => p).First();
+        }
     }
 
 }
